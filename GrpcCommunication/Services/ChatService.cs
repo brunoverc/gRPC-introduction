5c21cd588f02ae30a6532bbcafb8223d7fc328aa
brunoverc <69854207+brunoverc@users.noreply.github.com>
2022-04-06 12:45:31 +0100

    Add project files.

---
+++ GrpcCommunication/Services/ChatService.cs
@@ -0,0 +1,52 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcCommunication.Server;
+
+namespace GrpcCommunication.Services
+{
+    public class ChatService : Chat.ChatBase
+    {
+        private readonly ILogger<ChatService> _logger;
+
+        public ChatService(ILogger<ChatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task SendMessage(IAsyncStreamReader<ClientToServerMessage> requestStream,
+            IServerStreamWriter<ServerToClientMessage> responseStream,
+            ServerCallContext context)
+        {
+            var clientToServerTask = ClientToServerPingHandlingAsync(requestStream, context);
+
+            var serverToClientTask = ServerToClientPingAsync(responseStream, context);
+
+            await Task.WhenAll(clientToServerTask, serverToClientTask);
+        }
+
+        private static async Task ServerToClientPingAsync(IServerStreamWriter<ServerToClientMessage> responseStream, ServerCallContext context)
+        {
+            var cont = 0;
+            while (!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new ServerToClientMessage
+                {
+                    Text = $"Server said hi {++cont} times",
+                    Timestamp = Timestamp.FromDateTime(DateTime.UtcNow)
+                });
+
+                await Task.Delay(1000 * 5);
+            }
+        }
+
+        private async Task ClientToServerPingHandlingAsync(IAsyncStreamReader<ClientToServerMessage> requestStream, ServerCallContext context)
+        {
+            while (await requestStream.MoveNext() &&
+                            !context.CancellationToken.IsCancellationRequested)
+            {
+                var message = requestStream.Current;
+                _logger.LogInformation("The client said: {Message}", message.Text);
+            }
+        }
+    }
+}
---
+++ GrpcCommunicationClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcCommunicationClient;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7106");
+var client = new Greeter.GreeterClient(channel);
+
+var repply = await client.SayHelloAsync(new HelloRequest { Name = "Communication test client Bliss Applications" });
+
+Console.WriteLine("Greeting: " + repply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
